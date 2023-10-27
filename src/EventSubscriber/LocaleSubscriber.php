1f3a3a2749d41de32018f5590aa643d39bf1146e
zakaribel <zakaribel@hotmail.com>
2023-10-27 15:10:13 +0200

    test webhook

--- src/EventSubscriber/LocaleSubscriber.php
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -20,12 +20,14 @@ class LocaleSubscriber implements EventSubscriberInterface
             return;
         }
 
-        // try to see if the locale has been set as a _locale routing parameter
         if ($locale = $request->attributes->get('_locale')) {
             $request->getSession()->set('_locale', $locale);
+        } elseif ($locale = $request->getSession()->get('_locale')) {
+            $request->setLocale($locale);
         } else {
-            // if no explicit locale has been set on this request, use one from the session
-            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+            // if no explicit locale has been set on this request or in session, use the default locale
+            $request->getSession()->set('_locale', $this->defaultLocale);
+            $request->setLocale($this->defaultLocale);
         }
     }
 
